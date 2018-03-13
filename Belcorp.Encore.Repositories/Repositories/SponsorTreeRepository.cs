f0f4f5a368d24d8c8c25d23b72aac2f0c4183e68
CARCOSSIGUAS <carcossiguas@gmail.com>
2018-03-13 18:01:26 -0500

    Release 3

---
+++ Belcorp.Encore.Repositories/Repositories/SponsorTreeRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Belcorp.Encore.Entities;
+using Belcorp.Encore.Data.Contexts;
+using System.Linq;
+using Belcorp.Encore.Entities.Entities;
+
+namespace Belcorp.Encore.Repositories.Repositories
+{
+    public class SponsorTreeRepository : Repository<SponsorTree>
+    {
+        protected readonly EncoreCommissions_Context _dbCommissions_Context;
+
+        public SponsorTreeRepository(EncoreCommissions_Context dbCommissions_Context) : base(dbCommissions_Context)
+        {
+            _dbCommissions_Context = dbCommissions_Context;
+        }
+
+        public void lista()
+        {
+            var sponsor = from st in _dbCommissions_Context.SponsorTree join
+                          stm in _dbCommissions_Context.SponsorTree on
+                          st.SortPath equals stm.SortPath
+                          select st;
+        }
+
+    }
+}
